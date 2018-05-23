3b3437d3a48db17759e4d626e5febd47799aebff
Roy Kim <rykim79@gmail.com>
2018-05-23 18:58:34 -0400

    added components

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/header";
+import Map from "./components/map";
+import List from "./components/list";
+
+
+const App = (props) => {
+  return (
+    <div>
+      <Header />
+      <Map />
+      <List />
+    </div>
+  );
+};
